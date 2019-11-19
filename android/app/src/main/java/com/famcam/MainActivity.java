f29c5eeb0659da04c5a73183e59e5f1cef453dee
Sandeep Kumar <sandeep@code-brew.com>
2019-11-19 16:59:42 +0530

    first commit

---
+++ android/app/src/main/java/com/famcam/MainActivity.java
@@ -0,0 +1,21 @@
+package com.famcam;
+
+import android.widget.LinearLayout;
+import android.view.Gravity;
+import com.facebook.react.ReactActivity;
+// import com.reactnativenavigation.controllers.SplashActivity;
+import com.reactnativenavigation.NavigationActivity;
+import android.content.Intent;
+
+public class MainActivity extends NavigationActivity {
+   
+  //  @Override
+  //  public LinearLayout createSplashLayout() {
+  //      LinearLayout view = new LinearLayout(this);
+
+  //      view.setBackgroundResource(R.drawable.launch_screen);
+  //      view.setGravity(Gravity.CENTER);
+  //      return view;
+  //  }
+
+ }
